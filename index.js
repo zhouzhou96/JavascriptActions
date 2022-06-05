e4eb7d81a4c2e5cb28ccd4931c86287a0104147c
zhouzhou96 <877247946@qq.com>
2022-06-05 23:29:19 +0800

    小改动

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ try {
 
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
-
-    core.debug('1111111111111111111111111111111111111111111111')
+    const nameToGreet1 = core.getInput('who-to-greet1');
+    console.log(`Hello ${nameToGreet1}!`);
+    const nameToGreet2 = core.getInput('who-to-greet2');
+    console.log(`Hello ${nameToGreet2}!`);
 
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
+    core.setOutput("time1", "time1");
+    core.setOutput("time2", "time2");
+    core.setOutput("time3", "time3");
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
