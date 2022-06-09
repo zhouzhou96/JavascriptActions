a81beacde3aba99dc53276929465a1556c69724f
zhouzhou96 <88653057+zhouzhou96@users.noreply.github.com>
2022-06-09 19:18:29 +0800

    增加env.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core')
+const env = require('./env.js')
 
     const time = "1111111111111111111111111";
     core.setOutput("time", time);
@@ -10,9 +11,11 @@ const core = require('@actions/core')
     const HELLOWORLD_JAR_VERSION = '0.0.1-SNAPSHOT'
     core.setOutput("HELLOWORLD_JAR_VERSION", HELLOWORLD_JAR_VERSION);
 
-    const HELLOWORLD_JAR_ADDERSS = 'http://artifactrepo-dev:8082/artifactory/int_maven_dev/com/study'
-    core.setOutput("HELLOWORLD_JAR_ADDERSS", HELLOWORLD_JAR_ADDERSS);
+    // const HELLOWORLD_JAR_ADDERSS = 'http://artifactrepo-dev:8082/artifactory/int_maven_dev/com/study'
+    const NEW_HELLOWORLD_JAR_ADDERSS = env.HELLOWORLD_JAR_ADDERSS
+    core.setOutput("HELLOWORLD_JAR_ADDERSS", NEW_HELLOWORLD_JAR_ADDERSS);
 
-    const RUNNER_REPOSITORY = '/appl/idf/script/ODC/repository'
-    core.setOutput("RUNNER_REPOSITORY", RUNNER_REPOSITORY);
+    // const RUNNER_REPOSITORY = '/appl/idf/script/ODC/repository'
+    const ENV_RUNNER_REPOSITORY = env.RUNNER_REPOSITORY
+    core.setOutput("RUNNER_REPOSITORY", ENV_RUNNER_REPOSITORY);
 
