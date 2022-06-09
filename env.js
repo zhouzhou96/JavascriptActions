56b4264c3b32fcbe90f643ed4d0ec3bf47d71c2d
zhouzhou96 <88653057+zhouzhou96@users.noreply.github.com>
2022-06-09 19:20:16 +0800

    增加env.js

---
+++ env.js
@@ -0,0 +1,6 @@
+const env = {
+    HELLOWORLD_JAR_ADDERSS:'http://artifactrepo-dev:8082/artifactory/int_maven_dev/com/study',
+    RUNNER_REPOSITORY:'/appl/idf/script/ODC/repository'
+}
+
+export default env
