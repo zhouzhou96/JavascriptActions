201e96c44dd422dd4bfe974c1b1a2dc69f60c5f4
zhouzhou96 <88653057+zhouzhou96@users.noreply.github.com>
2022-06-10 10:02:10 +0800

    main应用using--修改

--- env.js
+++ env.js
@@ -2,4 +2,3 @@ module.exports = {
     HELLOWORLD_JAR_ADDERSS:'http://artifactrepo-dev:8082/artifactory/int_maven_dev/com/study',
     RUNNER_REPOSITORY:'/appl/idf/script/ODC/repository'
 }
-
--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const core = require('@actions/core')
 const env = require('./env.js')
 
-    const time = "1111111111111111111111111";
-    core.setOutput("time", time);
+const time = "1111111111111111111111111";
+core.setOutput("time", time);
 
 
-    const HELLOWORLD_JAR_NAME = 'helloWorld'
-    core.setOutput("HELLOWORLD_JAR_NAME", HELLOWORLD_JAR_NAME);
+const HELLOWORLD_JAR_NAME = 'helloWorld'
+core.setOutput("HELLOWORLD_JAR_NAME", HELLOWORLD_JAR_NAME);
 
-    const HELLOWORLD_JAR_VERSION = '0.0.1-SNAPSHOT'
-    core.setOutput("HELLOWORLD_JAR_VERSION", HELLOWORLD_JAR_VERSION);
+const HELLOWORLD_JAR_VERSION = '0.0.1-SNAPSHOT'
+core.setOutput("HELLOWORLD_JAR_VERSION", HELLOWORLD_JAR_VERSION);
 
-    // const HELLOWORLD_JAR_ADDERSS = 'http://artifactrepo-dev:8082/artifactory/int_maven_dev/com/study'
-    const NEW_HELLOWORLD_JAR_ADDERSS = env.HELLOWORLD_JAR_ADDERSS
-    core.setOutput("HELLOWORLD_JAR_ADDERSS", NEW_HELLOWORLD_JAR_ADDERSS);
+// const HELLOWORLD_JAR_ADDERSS = 'http://artifactrepo-dev:8082/artifactory/int_maven_dev/com/study'
+const NEW_HELLOWORLD_JAR_ADDERSS = env.HELLOWORLD_JAR_ADDERSS
+core.setOutput("HELLOWORLD_JAR_ADDERSS", NEW_HELLOWORLD_JAR_ADDERSS);
 
-    // const RUNNER_REPOSITORY = '/appl/idf/script/ODC/repository'
-    const ENV_RUNNER_REPOSITORY = env.RUNNER_REPOSITORY
-    core.setOutput("RUNNER_REPOSITORY", ENV_RUNNER_REPOSITORY);
-
+// const RUNNER_REPOSITORY = '/appl/idf/script/ODC/repository'
+const ENV_RUNNER_REPOSITORY = env.RUNNER_REPOSITORY
+core.setOutput("RUNNER_REPOSITORY", ENV_RUNNER_REPOSITORY);
